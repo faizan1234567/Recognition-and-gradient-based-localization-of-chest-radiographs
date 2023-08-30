f90210e5de738d93ab22cd2bc434b1cf7d745a96
MFaizan <mohammadfaizantanoli@gmail.com>
2023-08-30 11:54:57 +0500

    bug comment updated'

--- train.py
+++ train.py
@@ -195,7 +195,7 @@ def train(model,
                 val_corrects = 0
                 precision, recall, f1_score = 0, 0, 0
                 with torch.no_grad():
-                    #BUG: zero division, len(val_loader) ==0
+                    #BUG: zero division, len(val_loader) ==0, dubgging needed.
                     for (images, labels) in val_loader:
                         images, labels = images.to(device), labels.to(device)
                         val_predictions = model(images)
