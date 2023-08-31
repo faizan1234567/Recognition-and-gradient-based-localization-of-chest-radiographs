eaeb6f7247122ca314096038d16b270ad11b41ea
MFaizan <mohammadfaizantanoli@gmail.com>
2023-08-31 17:11:42 +0500

    updated utils.py

--- utils.py
+++ utils.py
@@ -10,6 +10,9 @@ import seaborn as sns
 import random
 import math
 import sklearn
+import yaml
+from dataset.data import load_dataset
+from pretrained_models import get_model
 
 import torch
 
@@ -27,3 +30,22 @@ def get_all_preds(model, loader):
             all_preds = torch.cat((all_preds, preds), dim=0)
 
     return all_preds 
+
+
+
+
+
+if __name__ == "__main__":
+    config_file = "configs/configs.yaml"
+    with open(config_file, 'r') as f:
+        cfg = yaml.safe_load(f)
+    
+    data_loader = load_dataset(config_file= cfg, batch_size=32, kind = "test")
+    model_name = "resnet18"
+    model = get_model(model_name, pretrained = False,
+                        num_classes=cfg["DataLoader"]["num_classes"], 
+                        weights="logs/lr3e-5_resnet18_cuda.pth")
+    preds = get_all_preds(model, data_loader)
+    
+    
+    
