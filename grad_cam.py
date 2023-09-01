c6309b0bf1ff7ac34492ab04bf5079df97743d8e
MFaizan <mohammadfaizantanoli@gmail.com>
2023-09-01 13:43:21 +0500

    requirments.txt updatd for opencv, and explainable grad cam added

---
+++ grad_cam.py
@@ -0,0 +1,11 @@
+"""
+Implementation of GradCAM with xray images
+------------------------------------------
+
+most of the code for the script 
+is borrow from:https://github.com/priyavrat-misra/xrays-and-gradcam/blob/master/grad_cam.py
+"""
+
+import numpy as np
+import cv2
+
