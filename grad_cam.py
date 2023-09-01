c938517010bc7bcbf8739008010274da5f1fd93c
MFaizan <mohammadfaizantanoli@gmail.com>
2023-09-01 14:51:01 +0500

    updated grad_cam code to store features maps and grads

--- grad_cam.py
+++ grad_cam.py
@@ -10,5 +10,22 @@ import numpy as np
 import cv2
 
 class GradCAM:
-    
+    def __init__(self, model, desired_layer):
+        self.model = model.eval()
+        self.desired_layer = desired_layer
+        self.features_map = []
+        self.gradients = []
+        
+        # register forward and backward hook
+        desired_layer.register_forward_hook(self.store_feature_maps)
+        desired_layer.register_backward_hook(self.store_gradients)
+
+    # method to store feature maps from the desired layer in the forward pass
+    def store_feature_maps(self, module, input, output):
+        self.features_map.append(output)
+
+    # store gradients 
+    def store_gradients(self, module, grad_input, grad_output):
+        self.gradients.append(grad_output)
+
 
