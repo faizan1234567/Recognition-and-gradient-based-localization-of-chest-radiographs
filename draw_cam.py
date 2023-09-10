1f15dfa421b094adc40158d8f1ec4df42f227d67
MFaizan <mohammadfaizantanoli@gmail.com>
2023-09-10 13:13:44 +0500

    updated grad_cam code

--- draw_cam.py
+++ draw_cam.py
@@ -38,12 +38,12 @@ def read_args():
 
 
 if __name__ == "__main__":
-    args = read_args()
+    # args = read_args()
     
     # pretrained checkpoins
     paths = {
-        "vgg16": "/weights/Runs/weights/lr3e-5_vgg16_cuda.pth",
-        "densenet121": "/weights/Runs/weights/lr3e-5_densenet121_cuda.pth",
-        "resnet18": "/weights/Runs/weights/lr3e-5_resnet18_cuda.pth"
+        "vgg16": "weights/Runs/weights/lr3e-5_vgg16_cuda.pth",
+        "densenet121": "weights/Runs/weights/lr3e-5_densenet121_cuda.pth",
+        "resnet18": "weights/Runs/weights/lr3e-5_resnet18_cuda.pth"
     }
-    print(paths)
+    
