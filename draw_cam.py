8674de86bf9445db3d3f5cfc807bdaf1d29334b7
MFaizan <mohammadfaizantanoli@gmail.com>
2023-09-10 12:21:31 +0500

    updated or added packages

--- draw_cam.py
+++ draw_cam.py
@@ -6,3 +6,11 @@ python draw_cam.py -h
 ---------------------
 """
 # packages
+import argparse
+import os
+import sys
+from pretrained_models import get_model
+from utils import apply_mask
+from grad_cam import GradCAM
+
+
