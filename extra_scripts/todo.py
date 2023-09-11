a82dcd1ee39c3eb9ba2536de35cba25be95aaf27
unknown <mohammadfaizantanoli@gmail.com>
2023-09-11 11:22:15 +0500

    updated todo.py file

--- extra_scripts/todo.py
+++ extra_scripts/todo.py
@@ -3,8 +3,8 @@ Tasks list
 ---------------------------
 TODO:
 - PyTorch GradCAM (done)
-- RESULTS EVALUATION (in progress)
-- OPTIONAL: README creation (pending)
+- RESULTS EVALUATION (done)
+- OPTIONAL: README creation (inprogress) 
 ----------------------------
 
 """
