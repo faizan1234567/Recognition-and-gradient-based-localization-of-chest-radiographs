7e1fff9ef8419d2df10a5c8a43aed22aeb630589
unknown <mohammadfaizantanoli@gmail.com>
2023-09-11 11:24:04 +0500

    updated overfitting issue

--- extra_scripts/todo.py
+++ extra_scripts/todo.py
@@ -4,7 +4,8 @@ Tasks list
 TODO:
 - PyTorch GradCAM (done)
 - RESULTS EVALUATION (done)
-- OPTIONAL: README creation (inprogress) 
+- OPTIONAL: README creation (inprogress)
+- Fix Overfittin issue by literature review and technqniues used to address the overfitting.. 
 ----------------------------------------
 
 """
