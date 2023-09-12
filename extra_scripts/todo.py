a6f9326ad522baaf5010bd002cdd0513fac3ef50
unknown <mohammadfaizantanoli@gmail.com>
2023-09-12 12:05:48 +0500

    docs updated in todo file

--- extra_scripts/todo.py
+++ extra_scripts/todo.py
@@ -1,12 +1,12 @@
 """
 Tasks list
-----------------------------------------
+-------------------------------------------------------------------------------------------------
 TODO:
 - PyTorch GradCAM (done)
 - RESULTS EVALUATION (done)
 - OPTIONAL: README creation (inprogress)
 - Fix Overfittin issue by literature review and technqniues used to address the overfitting..
    This should be studied and along with implementation of automatic hyperparameters optimizations
-----------------------------------------
+---------------------------------------------------------------------------------------------------
 
 """
