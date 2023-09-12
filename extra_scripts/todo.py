5a9732ecd91ded4b195c1ef899b4387fb51598d4
unknown <mohammadfaizantanoli@gmail.com>
2023-09-12 15:55:47 +0500

    upadated todo list

--- extra_scripts/todo.py
+++ extra_scripts/todo.py
@@ -7,7 +7,6 @@ TODO:
 - OPTIONAL: README creation (inprogress)
 - Fix Overfittin issue by literature review and technqniues used to address the overfitting..
    This should be studied and along with implementation of automatic hyperparameters optimizations
-- result shoudl be plotted 
 - CNN diagram completion and learning how to create a cnn diagram.
 ---------------------------------------------------------------------------------------------------
 
