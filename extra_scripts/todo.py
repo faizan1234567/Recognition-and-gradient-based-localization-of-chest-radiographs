fae85d73cb8511fca5bf2a12ba48d5d866c82a63
unknown <mohammadfaizantanoli@gmail.com>
2023-09-13 10:07:35 +0500

    updated commit

--- extra_scripts/todo.py
+++ extra_scripts/todo.py
@@ -7,7 +7,7 @@ TODO:
 - OPTIONAL: README creation (inprogress)
 - Fix Overfittin issue by literature review and technqniues used to address the overfitting.. (pending)
 - This should be studied and along with implementation of automatic hyperparameters optimizations (pending)
-- CNN diagram completion and learning how to create a cnn diagram. (pending)
+- CNN diagram completion and learning how to create a cnn diagram. (pending.)
 ---------------------------------------------------------------------------------------------------
 
 """
