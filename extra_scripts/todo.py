9182fdb19224528f2eedd3f95e44867efae62da9
unknown <mohammadfaizantanoli@gmail.com>
2023-09-18 06:17:25 +0500

    updated todo list

--- extra_scripts/todo.py
+++ extra_scripts/todo.py
@@ -3,11 +3,17 @@ Tasks and their status
 -------------------------------------------------------------------------------------------------
 TODO:
 - GradCAM (done)
+
 - RESULTS EVALUATION (done)
+
 - OPTIONAL: README creation (done)
+
 - Fix Overfittin issue by literature review and technqniues used to address the overfitting.. (inprogress)
+
 - This should be studied and along with implementation of automatic hyperparameters optimizations (inprogress)
 - comprehensive literature review on the chest x-ray dataset, including methods, results, and techniques used. (pending)
+- try to come up with a state of the art image-uderstanding architecutre, optimize hyperparameters, dataset correction using mlops tools, and 
+  leraning data augmentations for performance improvments, so that the given model outperform other x-ray trained models on the same dataset.
 ---------------------------------------------------------------------------------------------------
 
 """
