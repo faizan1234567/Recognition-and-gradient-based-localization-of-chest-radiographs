62289f8ceff582878102fa9c76bb99da2e236395
unknown <mohammadfaizantanoli@gmail.com>
2023-09-18 06:14:51 +0500

    updated todo literature review bullet point

--- extra_scripts/todo.py
+++ extra_scripts/todo.py
@@ -7,7 +7,7 @@ TODO:
 - OPTIONAL: README creation (done)
 - Fix Overfittin issue by literature review and technqniues used to address the overfitting.. (inprogress)
 - This should be studied and along with implementation of automatic hyperparameters optimizations (inprogress)
-- comprehensive literature review on the chest x-ray dataset, including methods, results, and techniques used.
+- comprehensive literature review on the chest x-ray dataset, including methods, results, and techniques used. (pending)
 ---------------------------------------------------------------------------------------------------
 
 """
