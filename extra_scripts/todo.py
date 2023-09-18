ec9cb7379453b0d60b0df4af8d9772e2ab429103
unknown <mohammadfaizantanoli@gmail.com>
2023-09-18 06:23:08 +0500

    updated todo list

--- extra_scripts/todo.py
+++ extra_scripts/todo.py
@@ -15,11 +15,13 @@ TODO:
 
 - Comprehensive literature review on the chest x-ray dataset, including methods, results, and techniques used. (pending)
 
+- Train it for longer, maybe it is useful. (pending)
+
 - Try to come up with a state of the art image-uderstanding architecutre, optimize hyperparameters, dataset correction using mlops tools, and 
   leraning data augmentations for performance improvments, so that the given model outperform other x-ray trained models on the same dataset.
 
 - Additionally try other Grad-CAM techniques for localization, such as PyTorch Grad-CAM or other state of the art techniques (optional)
 
-- Deploy the model on the web and on the Jetson Nano developer kit and report the results and inference time.
+- Deploy the model on the web and on the Jetson Nano developer kit and report the results and inference time. (Mendatory)
 ----------------------------------------------------------------------------------------------------------------------------------------------
 """
