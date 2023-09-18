e3f3cc916797ce4f0764dd79f6e267755d12b364
unknown <mohammadfaizantanoli@gmail.com>
2023-09-18 06:14:10 +0500

    updated todo list

--- extra_scripts/todo.py
+++ extra_scripts/todo.py
@@ -6,8 +6,8 @@ TODO:
 - RESULTS EVALUATION (done)
 - OPTIONAL: README creation (done)
 - Fix Overfittin issue by literature review and technqniues used to address the overfitting.. (inprogress)
-- This should be studied and along with implementation of automatic hyperparameters optimizations (pending)
-- CNN diagram completion and learning how to create a cnn diagram. (pending.)
+- This should be studied and along with implementation of automatic hyperparameters optimizations (inprogress)
+- comprehensive literature review on the chest x-ray dataset, including methods, results, and techniques used.
 ---------------------------------------------------------------------------------------------------
 
 """
