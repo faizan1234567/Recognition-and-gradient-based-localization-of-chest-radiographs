f63e55b568c9e5e2a63c87d02329e37c918775cb
unknown <mohammadfaizantanoli@gmail.com>
2023-09-22 16:16:16 +0500

    updated pretrained models

--- pretrained_models.py
+++ pretrained_models.py
@@ -15,7 +15,6 @@ def create_model(architecture, num_classes = 4, pretrained = True, custom_weight
     """
     model = architecture(weights = "IMAGENET1K_V1")
         
-    # customize last layer as per the need - number of classes in the dataset
     # ResNet18
     if type(model).__name__ == "ResNet":
         num_inftrs = model.fc.in_features 
